b4faca6ea8e5c68662e95990b961d732dfcc0cf3
ddolsoon <minseong6284@naver.com>
2019-03-01 21:19:48 +0900

    문자열 덧셈 계산기를 통한 TDD 연습

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,10 +1,38 @@
 package calculator;
 
+import java.util.List;
+
 public class Calculator {
+
+    private List<Integer> numberList;
+
+    boolean isEmptyText(String text) {
+
+        if (text == null) {
+            return true;
+        }
+
+        if (text == "") {
+            return true;
+        }
+
+        return false;
+    }
 
     public int add(String text) {
 
+        int result = 0;
 
-        return 0;
+        if(isEmptyText(text)) {
+            return 0;
+        }
+
+        numberList = StringParser.parse(text);
+
+        for (int i = 0;i < numberList.size(); i++) {
+            result += numberList.get(i);
+        }
+
+        return result;
     }
 }
